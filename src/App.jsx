7c1942716b00791402900ca5b08c5a2f7b769b7d
diya sharma <sdiya0523@gmail.com>
2025-12-18 22:00:21 +0530

    Update Todo component

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App=()=>
 {/* <Products/> */}
 {/* <Slider/> */}
 {/* <Productss/> */}
-<Portfolio/>
+{/* <Portfolio/> */}
 {/* <Ustate/> */}
 {/* <TestimonialSlider/> */}
 {/* <UseReducer/> */}
